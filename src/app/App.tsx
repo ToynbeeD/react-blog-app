9489e2101c075b18e1cdf04f086a992c59a9c4ab
Денис Кожевников <newlife_kod@mail.ru>
2023-02-08 21:05:12 +0300

    create AppRoutes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,10 @@
-import { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 import './styles/index.scss'
 
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-
-import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -18,12 +14,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOOGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     )
 }

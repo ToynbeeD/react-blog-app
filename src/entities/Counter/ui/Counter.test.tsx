ebff40a558a19367325159a41a48b00ef4de516b
Денис Кожевников <newlife_kod@mail.ru>
2023-03-19 16:11:09 +0300

    added store and first entity

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react';
+import { ComponentRender } from 'shared/lib/tests';
+import { userEvent } from '@storybook/testing-library';
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+  test('Test render', () => {
+    ComponentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+    expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+  });
+
+  test('decrement', () => {
+    ComponentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+
+    userEvent.click(screen.getByTestId('decrement-btn'));
+    expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+  });
+
+  test('increment', () => {
+    ComponentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+
+    userEvent.click(screen.getByTestId('increment-btn'));
+    expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+  });
+});
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,18 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
 
 render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ErrorBoundary>
-  </BrowserRouter>,
+  <StoreProvider>
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  </StoreProvider>,
   document.getElementById('root'),
 );

4c066e02b02f4df3dd01dae46e99780f2d386264
Денис Кожевников <newlife_kod@mail.ru>
2023-03-25 01:15:55 +0300

    added user entity, login form

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -0,0 +1,26 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib';
+import { Button } from 'shared/ui/Button';
+import { Input } from 'shared/ui/Input';
+import cls from './LoginForm.module.scss';
+
+interface LoginFormProps {
+  className?: string;
+}
+
+export const LoginForm = (props: LoginFormProps) => {
+  const { className } = props;
+
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(cls.loginForm, {}, [className])}>
+      <Input type="text" className={cls.input} placeholder={t('Введите username')} autofocus />
+      <Input type="text" className={cls.input} placeholder={t('Введите пароль')} />
+
+      <Button className={cls.loginBtn}>
+        {t('Войти')}
+      </Button>
+    </div>
+  );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,9 @@
+import { LoginModal } from 'features/AuthByUsername';
 import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib';
 import { Button } from 'shared/ui/Button';
 import { ButtonTheme } from 'shared/ui/Button/Button';
-import { Modal } from 'shared/ui/Modal';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,8 +14,12 @@ export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
   const [isAuthModal, setIsAuthModal] = useState(false);
 
-  const onToggleModal = useCallback(() => {
-    setIsAuthModal((prev) => !prev);
+  const onCloseModal = useCallback(() => {
+    setIsAuthModal(false);
+  }, []);
+
+  const onShowModal = useCallback(() => {
+    setIsAuthModal(true);
   }, []);
 
   return (
@@ -23,16 +27,15 @@ export const Navbar = ({ className }: NavbarProps) => {
       <Button
         theme={ButtonTheme.CLEAR_INVERTED}
         className={cls.links}
-        onClick={onToggleModal}
+        onClick={onShowModal}
       >
         {t('Войти')}
       </Button>
 
-      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-        { /* eslint-disable i18next/no-literal-string */ }
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Delectus ipsam provident ea mollitia iste alias reiciendis nam cumque dolore eaque.
-      </Modal>
+      <LoginModal
+        isOpen={isAuthModal}
+        onClose={onCloseModal}
+      />
     </div>
   );
 };

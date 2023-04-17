d498265a6e1cb8518c769735fa147567884ec632
Денис Кожевников <newlife_kod@mail.ru>
2023-04-17 21:54:37 +0300

    added articles view selectors, articles fetching

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,50 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import ListIcon from 'shared/assets/icons/list.svg';
+import TiledIcon from 'shared/assets/icons/tiled.svg';
+import { Button } from 'shared/ui/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+import { ButtonTheme } from 'shared/ui/Button/Button';
+import { ArticleView } from '../../model/types/article';
+import cls from './ArticleViewSelector.module.scss';
+
+interface ArticleViewSelectorProps {
+  className?: string;
+  view: ArticleView;
+  onViewClick?: (view: ArticleView) => void;
+}
+
+const viewTypes = [
+  {
+    view: ArticleView.GRID,
+    icon: TiledIcon,
+  },
+  {
+    view: ArticleView.LIST,
+    icon: ListIcon,
+  },
+];
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+  const { className, view, onViewClick } = props;
+
+  const onClick = (newView: ArticleView) => () => {
+    onViewClick?.(newView);
+  };
+
+  return (
+    <div className={classNames(cls.articleViewSelector, {}, [className])}>
+      {viewTypes.map((type) => (
+        <Button
+          theme={ButtonTheme.CLEAR}
+          onClick={onClick(type.view)}
+        >
+          <Icon
+            Svg={type.icon}
+            className={classNames('', { [cls.notSelected]: type.view !== view })}
+          />
+        </Button>
+      ))}
+    </div>
+  );
+});

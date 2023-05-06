b6668c25986d892d8377eed1ceefb94da5cadbb3
Денис Кожевников <newlife_kod@mail.ru>
2023-05-06 21:39:29 +0300

    add eslint plugin for unused imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     '@typescript-eslint',
     'i18next',
     'toynbee-fsd',
+    'unused-imports',
   ],
   rules: {
     'max-len': ['error', { code: 120, ignoreComments: true }],
@@ -37,6 +38,7 @@ module.exports = {
     ],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
+    'unused-imports/no-unused-imports': 'error',
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'no-unused-vars': 'off',
